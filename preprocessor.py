5a1998d036863a361c212009649f8d2601742b92
gauthamarcot <gauthamarcot@yahoo.com>
2021-04-06 00:17:45 +0530

    requirements added and python folders create

--- preprocessor.py
+++ preprocessor.py
@@ -12,3 +12,27 @@ from facenet_pytorch import MTCNN
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Running on device : {}' .format(device))
+
+fp=open('metadata.json',)
+data=json.load(fp)
+
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+if not os.path.exists("data/train"):
+    os.makedirs("data/train")
+
+if not os.path.exists("data/train/real"):
+    os.makedirs("data/train/real")
+
+if not os.path.exists("data/train/fake"):
+    os.makedirs("data/train/fake")
+
+if not os.path.exists('data/val'):
+    os.makedirs('data/val')
+
+if not os.path.exists('data/val/real'):
+    os.makedirs('data/val/real')
+
+if not os.path.exists('data/val/fake'):
+    os.makedirs('data/val/fake')
