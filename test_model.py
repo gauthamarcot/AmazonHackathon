6f0a1495c6e14fa622562ed10b31f81f9e07a6e3
gauthamarcot <gauthamarcot@yahoo.com>
2021-04-08 14:48:12 +0530

    model rendered and acheived the test results

--- test_model.py
+++ test_model.py
@@ -1,1 +1,154 @@
-import keras
+import torch
+from PIL import Image
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+from facenet_pytorch import MTCNN
+import cv2
+from tqdm import tqdm
+import os
+import copy
+import math
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Running on device: {}'.format(device))
+
+#for training :data normalization & augmentation
+#for validation: data normalization
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomRotation(5),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomResizedCrop(224, scale=(0.96, 1.0), ratio=(0.95, 1.05)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize([224,224]),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+
+data_dir = "data"
+check_point_path = "checkpoint.pth"
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),data_transforms[x]) for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=90,shuffle=True, num_workers=1)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(class_names) # => ['fake', 'real']
+print(f'Train image size: {dataset_sizes["train"]}')
+print(f'Validation image size: {dataset_sizes["val"]}')
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=2, checkpoint=None):
+    since = time.time()
+
+    if checkpoint is None:
+        best_model_wts = copy.deepcopy(model.state_dict())
+        best_loss = math.inf
+        best_acc = 0.
+    else:
+        print(f'Val loss: {checkpoint["best_val_loss"]}, Val accuracy: {checkpoint["best_val_accuracy"]}')
+        model.load_state_dict(checkpoint['model_state_dict'])
+        best_model_wts = copy.deepcopy(model.state_dict())
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        best_loss = checkpoint['best_val_loss']
+        best_acc = checkpoint['best_val_accuracy']
+
+    for epoch in range(1, num_epochs + 1):
+        print('Epoch {}/{}'.format(epoch, num_epochs))
+        print('--' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for i, (inputs, labels) in enumerate(dataloaders[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward propogation
+
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward propogation and optimize only if in 'train' mode
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Accuracy: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_loss < best_loss:
+                print(f'New best model found!')
+                print(f'New record loss: {epoch_loss}, Previous record loss: {best_loss}')
+                best_loss = epoch_loss
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    print('Best val Accuracy: {:.4f} Best val loss: {:.4f}'.format(best_acc, best_loss))
+
+    # load the best model weights
+    model.load_state_dict(best_model_wts)
+    return model, best_loss, best_acc
+
+
+# BEGIN TRAINING PROCESS
+
+# if checkpoint is present with previously trained weights, load it
+try:
+    checkpoint = torch.load(check_point_path)
+    print("checkpoint loaded")
+except:
+    checkpoint = None
+    print("checkpoint not found")
+
+# calling the training function to train pre-trained ResNet50
+model_conv, best_val_loss, best_val_acc = train_model(model_conv,
+                                                      criterion,
+                                                      optimizer_conv,
+                                                      exp_lr_scheduler,
+                                                      num_epochs=50,
+                                                      checkpoint=checkpoint)
+
+torch.save({'model_state_dict': model_conv.state_dict(),
+            'optimizer_state_dict': optimizer_conv.state_dict(),
+            'best_val_loss': best_val_loss,
+            'best_val_accuracy': best_val_acc,
+            'scheduler_state_dict': exp_lr_scheduler.state_dict(),
+            }, check_point_path)
